7b37fb5b6755035f0a888467d9b64e4d559f099e
markromanov <romanovbvc@gmail.com>
2025-02-10 22:30:40 +0300

    feat: update merchandise list according to requirements

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -33,7 +33,13 @@ CREATE TABLE IF NOT EXISTS user_merch (
 
 -- Добавляем начальные товары
 INSERT INTO merch_items (name, price) VALUES
-    ('t-shirt', 50),
-    ('hoodie', 100),
-    ('cap', 30),
-    ('stickers', 10); 
+    ('t-shirt', 80),
+    ('cup', 20),
+    ('book', 50),
+    ('pen', 10),
+    ('powerbank', 200),
+    ('hoody', 300),
+    ('umbrella', 200),
+    ('socks', 10),
+    ('wallet', 50),
+    ('pink-hoody', 500); 
